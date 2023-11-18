01165a919f0fa184c51eb6dbbd416541955d7cf1
i3elj <guisande@gmail.com>
2023-11-18 17:37:53 -0300

    initial commit

---
+++ pages/homepage.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>Petrolina</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="pages/homepage.css"/>
+    </head>
+    <body>
+	<div id="fixed-position">
+	    <img id="nuvem1" class="nuvem" src="assets/nuvem1.png" />
+	    <img id="nuvem2" class="nuvem" src="assets/nuvem2.png" />
+	    <img id="nuvem3" class="nuvem" src="assets/nuvem3.png" />
+	    <img id="nuvem4" class="nuvem" src="assets/nuvem4.png" />
+	    <img id="nuvem5" class="nuvem" src="assets/nuvem5.png" />
+	    <img id="nuvem6" class="nuvem" src="assets/nuvem6.png" />
+	</div>
+
+	<!-- info content -->
+	<aside id="sidebar-info">
+	    <div id="close-btn-wrapper" onclick="toggleClass('#sidebar-info', 'sidebar-info-expanded')">
+		<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+		    <path d="M1 25L25 1M25 25L1 1" stroke="black" stroke-width="2" stroke-linecap="round"/>
+		</svg>
+	    </div>
+	    <h2>Title</h2>
+	    <div id="sidebar-info-content">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mauris commodo quis imperdiet. Tristique magna sit amet purus gravida quis blandit turpis. Cursus turpis massa tincidunt dui ut. Blandit cursus risus at ultrices. Vitae proin sagittis nisl rhoncus mattis. Non quam lacus suspendisse faucibus interdum. Erat nam at lectus urna duis convallis convallis. Orci eu lobortis elementum nibh tellus molestie nunc non. Amet nulla facilisi morbi tempus iaculis urna id. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam.</p>
+		<br/>
+		<p>
+		    Nibh praesent tristique magna sit amet purus gravida quis. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Cras semper auctor neque vitae tempus quam. Egestas congue quisque egestas diam. A scelerisque purus semper eget duis at tellus at urna. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Vel turpis nunc eget lorem dolor sed viverra ipsum. Nam at lectus urna duis convallis convallis tellus. Et netus et malesuada fames ac. Pellentesque sit amet porttitor eget dolor morbi non arcu. Ultrices tincidunt arcu non sodales neque sodales. Arcu dui vivamus arcu felis bibendum ut tristique. In est ante in nibh mauris.
+		    Leo integer malesuada nunc vel risus. Neque convallis a cras semper auctor neque vitae tempus quam. Potenti nullam ac tortor vitae purus faucibus ornare. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Ultricies tristique nulla aliquet enim tortor at. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Non sodales neque sodales ut etiam sit amet nisl. Cras semper auctor neque vitae. Tempus iaculis urna id volutpat. Eget magna fermentum iaculis eu non diam. Vel fringilla est ullamcorper eget nulla facilisi etiam. Gravida arcu ac tortor dignissim convallis aenean et. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Arcu vitae elementum curabitur vitae nunc sed velit dignissim. Diam sollicitudin tempor id eu. Diam sollicitudin tempor id eu nisl nunc. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet. Duis ultricies lacus sed turpis tincidunt. Elit ut aliquam purus sit amet luctus venenatis. Ultricies leo integer malesuada nunc vel.
+		</p>
+		<br/>
+		<p>
+		    Mattis enim ut tellus elementum sagittis vitae et leo duis. Porttitor eget dolor morbi non. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Erat velit scelerisque in dictum non consectetur a. Arcu dictum varius duis at consectetur lorem donec massa. Lacus luctus accumsan tortor posuere. Enim praesent elementum facilisis leo vel fringilla est. Egestas sed tempus urna et pharetra pharetra. At tempor commodo ullamcorper a lacus vestibulum sed arcu non. Etiam dignissim diam quis enim. Viverra aliquet eget sit amet tellus cras. Sagittis purus sit amet volutpat consequat. Nullam ac tortor vitae purus. Est velit egestas dui id ornare arcu odio ut. Volutpat ac tincidunt vitae semper quis lectus nulla.
+		    Integer eget aliquet nibh praesent tristique. Interdum varius sit amet mattis vulputate enim. Enim nec dui nunc mattis enim ut tellus elementum. Est velit egestas dui id ornare arcu odio ut. Nec ullamcorper sit amet risus nullam eget felis eget. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Adipiscing at in tellus integer feugiat scelerisque varius. Arcu bibendum at varius vel pharetra vel turpis nunc eget. Eu tincidunt tortor aliquam nulla facilisi. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Id nibh tortor id aliquet lectus proin nibh nisl condimentum. Felis imperdiet proin fermentum leo. In ornare quam viverra orci. Nec feugiat in fermentum posuere urna nec tincidunt. Imperdiet massa tincidunt nunc pulvinar. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Aliquam ultrices sagittis orci a scelerisque. Amet massa vitae tortor condimentum lacinia quis. Dignissim enim sit amet venenatis urna cursus eget nunc. Ullamcorper dignissim cras tincidunt lobortis.
+		</p>
+	    </div>
+	</aside>
+	
+	
+	<main>
+	    <div id="first-section">
+		<h1 class="title">Petrolina...</h1>
+		<p class="origin-text">A vila recebeu a denominação de Petrolina em homenagem ao Imperador D. Pedro II, que ocupava, então, o trono do Brasil. Há uma versão segundo a qual o topônimo seria uma dupla homenagem, com a junção do nome do imperador, em sua forma latina (Petrus), ao da imperatriz Tereza Cristina, resultando em Petrolina. Outra versão sugere que o topônimo teria sido derivado de “pedra linda”, expressão dada a uma pedra que havia na margem do rio, ao lado da matriz, e que foi utilizada nas obras de cantaria da catedral de Petrolina, um dos maiores monumentos históricos da cidade.</p>
+	    </div>
+
+	    <div id="second-section" >
+		<ul id="upper-years">
+		    <li onclick="console.log('clicked 1870')">1870</li>
+		    <li onclick="console.log('clicked 2010')">2010</li>
+		    <li onclick="console.log('clicked 2012')">2012</li>
+		    <li onclick="console.log('clicked 2014')">2014</li>
+		    <li onclick="console.log('clicked 2016')">2016</li>
+		    <li onclick="console.log('clicked 2018')">2018</li>
+		    <li onclick="console.log('clicked 2020')">2020</li>
+		    <li onclick="console.log('clicked 2022')">2022</li>
+		</ul>
+		<svg id="timeline" width="739" height="74" viewBox="0 0 739 74" fill="black">
+		    <path d="M6.5 37.0812V38.0812H7.5H58.5V59.1576C55.3849 59.6386 53 62.3314 53 65.5812C53 69.171 55.9101 72.0812 59.5 72.0812C63.0899 72.0812 66 69.171 66 65.5812C66 62.3314 63.6151 59.6386 60.5 59.1576V38.0812H103.5H154.5V59.1576C151.385 59.6386 149 62.3314 149 65.5812C149 69.171 151.91 72.0812 155.5 72.0812C159.09 72.0812 162 69.171 162 65.5812C162 62.3314 159.615 59.6386 156.5 59.1576V38.0812H199.5H250.5V59.1576C247.385 59.6386 245 62.3314 245 65.5812C245 69.171 247.91 72.0812 251.5 72.0812C255.09 72.0812 258 69.171 258 65.5812C258 62.3314 255.615 59.6386 252.5 59.1576V38.0812H295.5H346.5V59.1576C343.385 59.6386 341 62.3314 341 65.5812C341 69.171 343.91 72.0812 347.5 72.0812C351.09 72.0812 354 69.171 354 65.5812C354 62.3314 351.615 59.6386 348.5 59.1576V38.0812H391.5H442.5V59.1576C439.385 59.6386 437 62.3314 437 65.5812C437 69.171 439.91 72.0812 443.5 72.0812C447.09 72.0812 450 69.171 450 65.5812C450 62.3314 447.615 59.6386 444.5 59.1576V38.0812H487.5H538.5V59.1576C535.385 59.6386 533 62.3314 533 65.5812C533 69.171 535.91 72.0812 539.5 72.0812C543.09 72.0812 546 69.171 546 65.5812C546 62.3314 543.615 59.6386 540.5 59.1576V38.0812H583.5H634.5V59.1576C631.385 59.6386 629 62.3314 629 65.5812C629 69.171 631.91 72.0812 635.5 72.0812C639.09 72.0812 642 69.171 642 65.5812C642 62.3314 639.615 59.6386 636.5 59.1576V38.0812H679.5H730.5L730.5 59.1576C727.385 59.6386 725 62.3314 725 65.5812C725 69.171 727.91 72.0812 731.5 72.0812C735.09 72.0812 738 69.171 738 65.5812C738 62.3314 735.615 59.6386 732.5 59.1576L732.5 37.0812V36.0812H731.5H680.5V14.0047C683.615 13.5237 686 10.831 686 7.58118C686 3.99133 683.09 1.08118 679.5 1.08118C675.91 1.08118 673 3.99133 673 7.58118C673 10.831 675.385 13.5237 678.5 14.0047V36.0812H635.5H584.5V14.0047C587.615 13.5237 590 10.831 590 7.58118C590 3.99133 587.09 1.08118 583.5 1.08118C579.91 1.08118 577 3.99133 577 7.58118C577 10.831 579.385 13.5237 582.5 14.0047V36.0812H539.5H488.5V14.0047C491.615 13.5237 494 10.831 494 7.58118C494 3.99133 491.09 1.08118 487.5 1.08118C483.91 1.08118 481 3.99133 481 7.58118C481 10.831 483.385 13.5237 486.5 14.0047V36.0812H443.5H392.5V14.0047C395.615 13.5237 398 10.831 398 7.58118C398 3.99133 395.09 1.08118 391.5 1.08118C387.91 1.08118 385 3.99133 385 7.58118C385 10.831 387.385 13.5237 390.5 14.0047V36.0812H347.5H296.5V14.0047C299.615 13.5237 302 10.831 302 7.58118C302 3.99133 299.09 1.08118 295.5 1.08118C291.91 1.08118 289 3.99133 289 7.58118C289 10.831 291.385 13.5237 294.5 14.0047V36.0812H251.5H200.5V14.0047C203.615 13.5237 206 10.831 206 7.58118C206 3.99133 203.09 1.08118 199.5 1.08118C195.91 1.08118 193 3.99133 193 7.58118C193 10.831 195.385 13.5237 198.5 14.0047V36.0812H155.5H104.5V14.0047C107.615 13.5237 110 10.831 110 7.58118C110 3.99133 107.09 1.08118 103.5 1.08118C99.9101 1.08118 97 3.99133 97 7.58118C97 10.831 99.3849 13.5237 102.5 14.0047V36.0812H59.5H8.5V15.0047C11.6151 14.5237 14 11.831 14 8.58118C14 4.99133 11.0899 2.08118 7.5 2.08118C3.91015 2.08118 1 4.99133 1 8.58118C1 11.831 3.38491 14.5237 6.5 15.0047V37.0812Z" stroke="black" stroke-width="1" stroke-linecap="round"/>
+		</svg>
+		<ul id="lower-years">
+		    <li onclick="console.log('clicked 2009')">2009</li>
+		    <li onclick="console.log('clicked 2011')">2011</li>
+		    <li onclick="console.log('clicked 2013')">2013</li>
+		    <li onclick="console.log('clicked 2015')">2015</li>
+		    <li onclick="console.log('clicked 2017')">2017</li>
+		    <li onclick="console.log('clicked 2019')">2019</li>
+		    <li onclick="console.log('clicked 2021')">2021</li>
+		    <li onclick="console.log('clicked Hoje')">Hoje</li>
+		</ul>
+	    </div>
+	</main>
+
+	<!-- menu sidebar -->
+	<div id="sidebar-btn" onclick="toggleClass('#sidebar', 'sidebar-expanded')">
+	    <svg width="32" height="21" viewBox="0 0 32 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M0 0H32V3H0V0Z" fill="black"/>
+		<path d="M0 9H32V12H0V9Z" fill="black"/>
+		<path d="M0 18H32V21H0V18Z" fill="black"/>
+	    </svg>
+	</div>
+	
+	<aside id="sidebar">
+	    <h2>Menu</h2>
+	    <div class="sidebar-item">
+		<p onclick="loadCategory(2)">Política</p>
+	    </div>
+	    <div class="sidebar-item">
+		<p onclick="loadCategory(3)">Atrações</p>
+	    </div>
+	    <div class="sidebar-item">
+		<p onclick="loadCategory(4)">Obituário</p>
+	    </div>
+	    <div class="sidebar-item">
+		<p onclick="loadCategory(5)">Inaugurações</p>
+	    </div>
+	    <div class="sidebar-item">
+		<p onclick="loadCategory(6)">Estatísticas</p>
+	    </div>
+	    <div class="sidebar-item">
+		<p onclick="loadCategory(1)">Resumo do Ano</p>
+	    </div>
+
+	    <div id="years-scroll-wrapper">
+		<ul id="years-scroll">
+		    <li onclick="console.log('clicked')">1870</li>
+		    <li onclick="console.log('clicked')">2009</li>
+		    <li onclick="console.log('clicked')">2010</li>
+		    <li onclick="console.log('clicked')">2011</li>
+		    <li onclick="console.log('clicked')">2012</li>
+		    <li onclick="console.log('clicked')">2013</li>
+		    <li onclick="console.log('clicked')">2014</li>
+		    <li onclick="console.log('clicked')">2015</li>
+		    <li onclick="console.log('clicked')">2016</li>
+		    <li onclick="console.log('clicked')">2017</li>
+		    <li onclick="console.log('clicked')">2018</li>
+		    <li onclick="console.log('clicked')">2019</li>
+		    <li onclick="console.log('clicked')">2021</li>
+		    <li onclick="console.log('clicked')">2020</li>
+		    <li onclick="console.log('clicked')">2022</li>
+		    <li onclick="console.log('clicked')">Hoje</li>
+		</ul>
+	    </div>
+	</aside>
+    </body>
+
+    <script type="text/javascript" src="pages/homepage.js"></script>
+</html>
